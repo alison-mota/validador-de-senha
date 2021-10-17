5686eb682c0806a77f7d07c9b4e431cd82507e11
alison-mota <alisonalvesmota@gmail.com>
2021-10-17 16:37:13 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/iti/validadordesenha/ValidadorDeSenhaApplication.kt
@@ -0,0 +1,11 @@
+package br.com.iti.validadordesenha
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ValidadorDeSenhaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ValidadorDeSenhaApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/iti/validadordesenha/ValidadorDeSenhaApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.iti.validadordesenha
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ValidadorDeSenhaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
